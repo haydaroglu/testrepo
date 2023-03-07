d935cd7ce1f6b5ba1aac5ac622f30ce5b7f836a6
Selin Haydaroğlu <64706499+haydaroglu@users.noreply.github.com>
2023-03-07 18:47:56 +0300

    Add files via upload

---
+++ asalmi.py
@@ -0,0 +1,14 @@
+sayi = int(input("sayi : "))
+
+asalmi = True
+
+if (sayi== 1):
+    asalmi=False
+for i in range(2, sayi):
+    if(sayi % i == 0):
+        asalmi = False
+        break
+if asalmi:
+    print("sayi asaldir.")
+else:
+    print("sayi asal değildir.")
